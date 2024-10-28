f5325fe7737cef798f284e41ff1ef408e294da91
hao <ahmed.ait.ouazzou@protonmail.com>
2024-10-28 19:16:47 +0100

    Added fastapi.py

---
+++ app/backend/fastapi_app.py
@@ -0,0 +1,36 @@
+# Import packages
+import os
+from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+# Import src modules
+from grid_computing_forecasting import config, dataset
+from grid_computing_forecasting.modeling import predict
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Load pickle object for test API
+df = dataset.load_pickle_file(
+    os.path.join(config.root_dir, "app/backend/df_sample.pkl")
+)
+
+
+app = FastAPI(title="MyApp", description="Grid Computing Forecasting")
+
+
+class Item(BaseModel):
+    job_id: int
+
+
+@app.post("/prediction")
+def grownet_prediction(request: Item):
+    job_id = request.job_id
+
+    result = predict.with_xgboost(
+        config.root_dir, "models/xgboost.pkl", df.iloc[job_id]
+    )
+
+    return {"prediction": float(result)}
